1f11005969f515fac514aaa4a9a677f9a65c489d
Fenhl <fenhl@fenhl.net>
2025-12-15 00:44:54 +0000

    Update mhmw cask

--- Casks/mhmw.rb
+++ Casks/mhmw.rb
@@ -1,6 +1,6 @@
 cask "mhmw" do
-  version "17.0.3"
-  sha256 "73063446a51087f25a0d004db105a851b6f9069974d38191d2bd42a192e3af60"
+  version "17.1.0"
+  sha256 "76d8bd557c3b3d8ecf54c98eb4e5529b52ddacd2a1a8128ebaf9d3fd9d7b1b83"
 
   url "https://github.com/midoshouse/ootr-multiworld/releases/download/v#{version}/multiworld-gui.dmg",
       verified: "github.com/midoshouse/ootr-multiworld/"
